9366e3a850b845e0b5a4468ae28f22f9c009c782
ddobson <ddobson445@gmail.com>
2017-08-19 10:37:29 -0400

    Add tests for authentication happy paths - Refactor user sign up - Test sign out - Test sign in

--- features/auth.feature
+++ features/auth.feature
@@ -1,8 +1,24 @@
-Feature: Authentication
+Feature: User Authentication
+  Users should be able to sign up, sign out, sign in
+  Users should see appropriate error messages when validations fail
+  Users should not proceed to dashboard when validations fail
 
-Scenario: User login
+# Happy path tests
+Scenario: User sign up
   Given I am on the homepage
-  When I click the login button
+  When I click the Sign Up link
+    And I enter my new user credentials
+    And I click to Sign Up
+  Then I should be signed in
+
+Scenario: User sign out
+  Given I am signed in
+  When I click the Sign Out link
+  Then I should be signed out
+
+Scenario: User sign in
+  Given I have a user account
+  When I click the Sign In link
     And I enter my user credentials
-    And I click to log in
-  Then I should be logged in
+    And I click to Sign In
+  Then I should be signed in
--- features/step_definitions/auth_steps.rb
+++ features/step_definitions/auth_steps.rb
@@ -1,21 +1,47 @@
-Given(/^I am on the homepage$/) do
+Given('I am on the homepage') do
   visit Capybara.app_host
 end
 
-When(/^I click the login button$/) do
-  click_link 'Sign In'
+Given('I am signed in') do
+  step 'I am on the homepage'
+  step 'I click the Sign Up link'
+  step 'I enter my new user credentials'
+  step 'I click to Sign Up'
 end
 
-When(/^I enter my user credentials$/) do
-  fill_in 'email', :with => ENV['EMAIL']
+Given('I have a user account') do
+  step 'I am signed in'
+  step 'I click the Sign Out link'
+end
+
+When(/^I click the (.*) link$/) do |link|
+  click_link "#{link}"
+end
+
+When(/^I enter my (.*) credentials$/) do |user|
+  if user == 'new user'
+    @username = Time.new.to_i.to_s
+    @email = "#{@username}@qa.com"
+
+    fill_in 'userName', with: @username
+    fill_in 'passwordConf', with: ENV['PASSWORD']
+  end
+
+  fill_in 'email', :with => @email
   fill_in 'password', :with => ENV['PASSWORD']
 end
 
-When(/^I click to log in$/) do
-  click_button 'Sign In'
+When(/^I click to (.*)$/) do |button|
+  click_button "#{button}"
 end
 
-Then(/^I should be logged in$/) do
+Then('I should be signed in') do
   expect(page).to have_current_path('/dashboard')
   expect(page).to have_content('Sign Out')
+end
+
+Then('I should be signed out') do
+  expect(page).to have_current_path('/')
+  expect(page).to have_content('Sign Up')
+  expect(page).to have_content('Sign In')
 end
