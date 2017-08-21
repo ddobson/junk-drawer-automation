4197de0f3853eeb2ac15e63cdb593ecb1666eab6
ddobson <ddobson445@gmail.com>
2017-08-21 11:13:26 -0400

    Add tests for links validations

--- features/links.feature
+++ features/links.feature
@@ -1,6 +1,7 @@
 Feature: Links
   Users should be able to add, delete, and copy a link
   Users should be able to view and hide link details
+  Users should see appropriate error messages when validations fail
 
 # Happy path testing
 Scenario: Add a link
@@ -30,3 +31,27 @@ Scenario: Copy a link's address
   Given I have an account with a link
   When I click the copy icon
   Then The link should be on my clipboard
+
+  # Error testing
+  Scenario Outline: Add an invalid link
+    Given I am signed in
+    When I click the New Link button
+      And I enter invalid link <detail> with value <value>
+      And I click the Create button
+    Then I should see a <error> <detail> message for my link
+
+  Examples:
+  | detail      | value                     | error       |
+  | destination |                           | required    |
+  | destination | imgur.com/gallery/4AiXzf8 | invalid     |
+  | destination | http://                   | invalid     |
+  | destination | imgur                     | invalid     |
+  | destination | www.google.com            | incomplete  |
+  | title       |                           | required    |
+  | title       | 1                         | invalid     |
+  | title       | very_long_string          | invalid     |
+  | slashtag    |                           | required    |
+  | slashtag    | -hyphen                   | invalid     |
+  | slashtag    | two words                 | invalid     |
+  | slashtag    | special!@#$%^&*()chars    | invalid     |
+  | slashtag    | very_long_string          | lengthy     |
--- features/step_definitions/links_steps.rb
+++ features/step_definitions/links_steps.rb
@@ -6,9 +6,33 @@ Given('I have an account with a link') do
 end
 
 When('I enter link info into the form') do
-  fill_in 'destination', with: 'http://imgur.com/gallery/4AiXzf8'
-  fill_in 'title', with: 'cash cat'
-  fill_in 'slashtag', with: 'cash-cat'
+  step 'I enter a valid destination detail'
+  step 'I enter a valid title detail'
+  step 'I enter a valid slashtag detail'
+end
+
+When(/^I enter invalid link (.*) with value (.*)$/) do |invalid_type, value|
+  link_inputs = ['destination', 'title', 'slashtag']
+  link_inputs.delete(invalid_type)
+
+  value = Array.new(256, 'a').join if value == 'very_long_string'
+
+  fill_in invalid_type, with: value
+
+  link_inputs.each do |link_input|
+    step "I enter a valid #{link_input} detail"
+  end
+end
+
+When(/^I enter a valid (.*) detail$/) do |type|
+  case type
+  when 'destination'
+    fill_in 'destination', with: 'http://imgur.com/gallery/4AiXzf8'
+  when 'title'
+    fill_in 'title', with: 'cash cat'
+  when 'slashtag'
+    fill_in 'slashtag', with: 'cash-cat'
+  end
 end
 
 When(/^I click the (.*) icon/) do |type|
@@ -45,4 +69,30 @@ end
 Then('The link should be on my clipboard') do
   clip = Clipboard.paste
   expect(page).to have_content(clip)
+end
+
+Then(/^I should see a (.*) message for my link$/) do |type|
+  case type
+  when 'required destination'
+    expect(page).to have_content('URL is required.')
+  when 'invalid destination'
+    expect(page)
+      .to have_content('You must use a valid URL. "http(s)://" should be included.')
+  when 'incomplete destination'
+    expect(page).to have_content('Destination: Url is not valid')
+  when 'required title'
+    expect(page).to have_content('Title is required.')
+  when 'invalid title'
+    expect(page).to have_content('Titles must be 3 - 255 characters.')
+  when 'required slashtag'
+    expect(page).to have_content('Slashtag is required.')
+  when 'invalid slashtag'
+    expect(page)
+      .to have_content('Slashtags may only contain alphanumeric characters, hyphens and underscores.')
+  when 'lengthy slashtag'
+    expect(page)
+      .to have_content('Slashtag: Value cannot be greater than 50 characters')
+  else
+    raise StandardError, "Error of type '#{type}' was not handled"
+  end
 end
